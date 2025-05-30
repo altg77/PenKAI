8e0c05350a6777e5475b271ed0dbf3a32ff3a8f6
altg77 <tiago.santos2712@gmail.com>
2025-05-30 00:50:15 -0300

    a1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mangaka_portal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
